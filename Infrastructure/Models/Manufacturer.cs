ffd5bfc26dd547b57d34828152d4a531add9452b
Richard Fry <RichardFry@users.noreply.github.com>
2024-08-07 08:41:25 +0300

    Added Manufacturer

---
+++ Infrastructure/Models/Manufacturer.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Infrastructure.Models
+{
+    public class Manufacturer
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [DisplayName("Manufacturer Name")]
+        public string? Name { get; set; }
+    }
+}
